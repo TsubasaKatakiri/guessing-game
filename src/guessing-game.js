f7a191b3f5966406fac48b98a96ea83d67375d4a
Tsubasa <katakiritsubasa@gmail.com>
2024-09-23 21:35:31 +0300

    feat: add solution to guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.candidate = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.candidate = Math.round((this.min + this.max) / 2);
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate
     }
 
     greater() {
-
+        this.min = this.candidate
     }
 }
 
